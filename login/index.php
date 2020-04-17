109d3f114b6ff72909945330610218f5ce1c7cf0
Muhammad Faqih AL As Ad <63689878+muhammadfaqihalasad@users.noreply.github.com>
2020-04-18 05:16:20 +0700

    Add files via upload

---
+++ login/index.php
@@ -0,0 +1,25 @@
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="login.css">
+    <title>Login</title>
+  </head>
+  <body>
+    <form action="proses-login.php" method="post">
+      <center>
+              <h1>Login Petugas</h1>
+      <table>
+        <tr>
+        <td><label for="">Username</label></td>
+        <td><input type="text" name="username" id=""></td>
+       </tr>
+       <tr>
+         <td><label for="">Password</td>
+           <td><input type="password" name="password" id=""></td>
+       </tr>
+       <td></td>
+       <td><input  class="login"  type="submit" value="Login" name="btnlogin"></td>
+      </table>
+    </form>
+  </body>
+</html>
