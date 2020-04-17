1a8f7af5baca6d15bb815a564f042efbd1d0da6c
Muhammad Faqih AL As Ad <63689878+muhammadfaqihalasad@users.noreply.github.com>
2020-04-18 05:23:08 +0700

    Add files via upload

---
+++ asset/header.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="http://localhost/siperpus/asset/Bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://localhost/siperpus/asset/fontawesome/css/all.min.css">
+
+    <title>SIPERPUS</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<a class="navbar-brand"href="#"><i class="fas fa-book-reader text-white mr-2"></i>SIPERPUS</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarNavAItMarkup">
+    <div class="navbar-nav">
+        <a class="nav-item nav-link" href="http://localhost/siperpus/index.php">Dashboard</a>
+        <a class="nav-item nav-link" href="http://localhost/siperpus/buku/index.php">Buku</a>
+        <a class="nav-item nav-link" href="http://localhost/siperpus/anggota/index.php">Anggota</a>
+        <a class="nav-item nav-link" href="http://localhost/siperpus/transaksi/index.php">Peminjaman</a>
+        <a class="nav-item nav-link" href="http://localhost/siperpus/login/logout.php">Logout</a>
+    </div>
+    </div>
+    </nav>
+    
+    <script src="http://localhost/siperpus/asset/jquery.js"></script>
+    <script src="http://localhost/siperpus/asset/bootstrap/js/bootstrap.min.js"></script>
+    
+</body>
+</html>
